8797815fa411d9ce899cd483c8deaa47df6fd29d
Gent Maksutaj <gent.maksutajmax@gmail.com>
2026-02-02 23:14:51 -0500

    edge cases files

---
+++ frontend/test_mine_bad.c
@@ -0,0 +1,35 @@
+extern void print(int);
+extern int read();
+
+int func(int i ) {
+    int a;
+    int b;
+    int c;
+    int papanewguinea;
+    a = 10;
+    b = 10;
+    c = 10;
+    {
+        int a;
+        int b;
+        int c;
+        a = 30;
+        b = 30;
+        c = 30;
+        {
+            int a;
+            int b;
+            a = 20;
+            b = 20;
+            if (a < 30) {
+                int a;
+                a = 20;
+            }
+
+        }
+    }
+    int d;
+    int e;
+    int f;
+    return 1;
+}
